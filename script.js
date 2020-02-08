be56fd9e13e5caa0f0f18f73961a0bf18cf4b9b1
John Frail Bongat <jfraildev@gmail.com>
2020-02-08 18:18:59 +0800

    Learned about scoping

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
-/**********
- * Hoisting
+// /**********
+//  * Hoisting
+//  */
+
+// calcAge(1995);
+
+// function calcAge(year) {
+//   console.log(2020 - year);
+// }
+
+// // retirement(1991); *does not work*
+
+// var retirement = function(year) {
+//   console.log(60 - (2020 - year));
+// };
+
+// retirement(1997);
+
+// // Variables
+
+// console.log(age);
+// var age = 23;
+
+// function foo() {
+//   var age = 65;
+//   console.log(age);
+// }
+
+// foo();
+// console.log(age);
+
+/*********
+ * Scoping
  */
 
-calcAge(1995);
+// // First scoping example
+// var a = 'Hello!';
+// first();
 
-function calcAge(year) {
-  console.log(2020 - year);
+// function first() {
+//   var b = 'Hi!';
+//   second();
+
+//   function second() {
+//     var c = 'Hey!';
+//     console.log(a, b, c);
+//   }
+// }
+
+// Second example to show difference between execution stack and scope chain
+var a = 'Hello!';
+first();
+
+function first() {
+  var b = 'Hi!';
+  second();
+
+  function second() {
+    var c = 'Hey!';
+    third();
+  }
 }
 
-// retirement(1991); *does not work*
-
-var retirement = function(year) {
-  console.log(60 - (2020 - year));
-};
-
-retirement(1997);
-
-// Variables
-
-console.log(age);
-var age = 23;
-
-function foo() {
-  var age = 65;
-  console.log(age);
+function third() {
+  var d = 'John';
+  // console.log(c);
+  console.log(a, d);
 }
-
-foo();
-console.log(age);
